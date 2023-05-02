07224f72335df9844eba6f21ec18514481004dd3
MausoofAzam <azam_7899@outlook.com>
2023-05-02 09:29:22 +0530

    second commit

--- Exam-Mcq/src/main/java/com/exam/controller/QuestionController.java
+++ Exam-Mcq/src/main/java/com/exam/controller/QuestionController.java
@@ -17,14 +17,11 @@ public class QuestionController {
 
     @Autowired
     private QuestionService questionService;
-
+/*
     @GetMapping("getByCategoryAndLevel/{category}/{level}")
     public List<Question> getByCatogoryAndLevel(@PathVariable String category, @PathVariable String level) {
         return questionService.getQuestionsByCategoryAndLevel(category, level);
-    }/*@GetMapping("getByCategoryAndLevel/{category}/{level}")
-    public List<Question> getByCatogoryAndLevel(@PathVariable String category, @PathVariable String level) {
-        return questionService.getQuestionsByCategoryAndLevel(category, level);
-    }*/
+    }
 
     @GetMapping("/")
     public String viewHomePage() {
@@ -56,22 +53,74 @@ public class QuestionController {
         return "result";
     }
 
-   /* @GetMapping("/addQuestion")
+   *//* @GetMapping("/addQuestion")
     public String showAddQuestionForm(Model model) {
         model.addAttribute("question", new Question());
         return "addQuestion";
-    }*/
+    }*//*
 
-    /*@PostMapping("/saveQuestion")
+    *//*@PostMapping("/saveQuestion")
     public String saveQuestion(@ModelAttribute("question") Question question,List<Option> options) {
         questionService.saveQuestion(question,options);
         return "redirect:/mcq/addQuestion";
-    }*/
+    }*//*
+    @GetMapping("/startExam")
+    public String startExam(@RequestParam(required = false) String category,
+                            @RequestParam(required = false) String level,
+                            Model model) {
+
+        // Check if category and level are present in the request parameters
+        if (category != null && level != null) {
+            // Fetch questions from the database based on the selected category and level
+            List<Question> questions = questionService.getQuestionsByCategoryAndLevel(category, level);
+            model.addAttribute("questions", questions);
+        }
+
+        return "exam";
+    }
 
     @PostMapping("/saveQuestion")
     public Question saveQuestion(@RequestBody Question question) {
         questionService.saveQuestion(question);
         return question;
+    }*/
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home";
     }
+
+    @GetMapping("/addQuestion")
+    public String addQuestion(Model model) {
+        model.addAttribute("question", new Question());
+        return "addQuestion";
+    }
+
+    @PostMapping("/addQuestion")
+    public String addQuestionSubmit(@ModelAttribute("question") Question question) {
+        questionService.save(question);
+        return "redirect:/addQuestion";
+    }
+
+    @GetMapping("/startExam")
+    public String startExam(Model model, @RequestParam("category") String category,
+                            @RequestParam("level") String level) {
+        List<Question> questions = questionService.getQuestionsByCategoryAndLevel(category, level);
+        model.addAttribute("questions", questions);
+        return "startExam";
+    }
+
+    @PostMapping("/submitExam")
+    public String submitExam(@RequestParam Map<String, String> answers) {
+        int score = examService.calculateScore(answers);
+        return "redirect:/result?score=" + score;
+    }
+
+    @GetMapping("/result")
+    public String showResult(@RequestParam("score") int score, Model model) {
+        model.addAttribute("score", score);
+        return "result";
+    }
+
 
 }
